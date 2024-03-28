23bf6739bab462dd46ae503ed74dba2892f15f0c
MuchammadNurFathoni22410100016 <163947012+Fathoni26@users.noreply.github.com>
2024-03-28 08:51:46 +0700

    upload tugas1
    upload tugas1 pbm

---
+++ tugas01.dart
@@ -0,0 +1,12 @@
+void main() {
+  var n = 79;
+  if (n >= 0 && n < 60) {
+    print('nilai C');
+  } else if (n >= 60 && n < 80) {
+    print('nilai B');
+  } else if (n >= 80 && n <= 100) {
+    print('nilai A');
+  } else {
+    print('kosong');
+  }
+}
